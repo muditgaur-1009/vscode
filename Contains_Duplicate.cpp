78963822eaf3825b3c60d202bb47a00123690241
muditgaur-1009 <91221391+muditgaur-1009@users.noreply.github.com>
2022-12-13 22:27:53 +0530

    third question

---
+++ Contains_Duplicate.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if (nums.empty()) { return false; }
+        unordered_map<int,int> mp;
+        for (int i : nums) {
+            if (++mp[i] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
